030a99350b565983c4b7362a9c006f6811d7f2d9
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-11-09 02:34:53 +0200

    Added to Main.java methods fox.info(), fish.info(), wolf.info()

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Fox fox = new Fox();
+        System.out.println("About fox:");
+        fox.info();
 
+        System.out.println("");
+
+        Fish fish = new Fish();
+        System.out.println("About fish:");
+        fish.info();
+
+        System.out.println("");
+
+        Wolf wolf = new Wolf(0);
+        System.out.println("About wolf:");
+        wolf.info();
     }
 }
