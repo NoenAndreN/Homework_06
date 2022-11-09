660fc1d47313333df2c5bca77836ffadcf806042
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-11-09 02:17:14 +0200

    Created class Fox that extends Mammals, added methods nameOfAnimal(), voice(), canBite(), paws(), info()

--- src/src/Mammals.java
+++ src/src/Mammals.java
@@ -32,3 +32,34 @@ class Wolf extends Mammals {
         System.out.println("Voice: " + this.voice());
     }
 }
+
+
+class Fox extends Mammals {
+
+
+    public String nameOfAnimal() {
+        return "Fox";
+    }
+
+    @Override
+    public String voice() {
+        return "What does the fox say?";
+    }
+
+
+    public boolean canBite() {
+        return true;
+    }
+
+
+    int paws() {
+        return 4;
+    }
+
+    public void info() {
+        System.out.println("Name: " + this.nameOfAnimal());
+        System.out.println("Can bite: " + this.canBite());
+        System.out.println("Has " + this.paws() + " paws.");
+        System.out.println("Voice: " + this.voice());
+    }
+}
