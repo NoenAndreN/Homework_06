2576651d86ec565f5703ae8b6e907f15c211f93f
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-11-09 02:14:01 +0200

    Created class Wolf that extends Mammals, added methods nameOfAnimal(), voice(), canBite(), paws(), info()

--- src/src/Mammals.java
+++ src/src/Mammals.java
@@ -5,3 +5,30 @@ abstract class Mammals implements Animals, Name {
         return "Голос!";
     }
 }
+
+
+class Wolf extends Mammals {
+    public String nameOfAnimal() {
+        return "Wolf";
+    }
+
+    @Override
+    public String voice() {
+        return "Wooo!";
+    }
+
+    public boolean canBite() {
+        return true;
+    }
+
+    int paws() {
+        return 4;
+    }
+
+    public void info() {
+        System.out.println("Name: " + this.nameOfAnimal());
+        System.out.println("Can bite: " + this.canBite());
+        System.out.println("Has " + this.paws() + " paws.");
+        System.out.println("Voice: " + this.voice());
+    }
+}
